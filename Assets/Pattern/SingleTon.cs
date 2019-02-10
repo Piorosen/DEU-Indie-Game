8cc860ebf9522e288c88b5546489fca739cdc6b3
Piorosen <aoikazto@gmail.com>
2019-02-10 12:45:17 +0900

    프로젝트 초기화.

---
+++ Assets/Pattern/SingleTon.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Pattern
+{
+    public class SingleTon<T> where T : class, new ()
+    {
+        private static volatile T _instance = null;
+
+        public static T Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new T();
+                }
+
+                return _instance;
+            }
+        }
+    }
+}
--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -5,11 +5,20 @@ using UnityEngine;
 
 public class InputManager : MonoBehaviour
 {
-    public static Queue<(KeyCode, int, bool)> queue = new Queue<(KeyCode, int, bool)>();
+    public Queue<(long Ticks, KeyCode Code)> Keys = new Queue<(long Ticks, KeyCode Code)>();
+
+    int Ticks = 0;
 
     // Update is called once per frame
     void Update()
     {
-
+        foreach (var key in Enum.GetValues(typeof(KeyCode)))
+        {
+            if (Input.GetKeyDown((KeyCode)key))
+            {
+                Keys.Enqueue((this.Ticks, (KeyCode)key));
+            }
+        }
+        Ticks++;
     }
 }
