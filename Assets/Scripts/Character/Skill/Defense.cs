e2f07fd3310bc79ac901b71983501f2f3127e3dd
Piorosen <aoikazto@gmail.com>
2019-02-10 13:27:26 +0900

    정리

--- Assets/Scripts/Character/Skill/Defense.cs
+++ Assets/Scripts/Character/Skill/Defense.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Defense : MonoBehaviour
+public class Defense : Skill
 {
     // Start is called before the first frame update
     void Start()
